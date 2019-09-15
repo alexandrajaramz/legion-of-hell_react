8d4f43abda7a04f937ea5565c4440ccc19954a78
alexandrajaramz <alexandra.jara.mz@gmail.com>
2019-09-15 20:40:25 +0200

    create file with user data and show the info

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import './App.css';
+import userData from './userData';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Buenas tardes y esas cosas.</h1>
-    </div>
-  );
+
+class App extends React.Component {
+/*   constructor(props) {
+    super(props);
+  } */
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className="app__title">Datos de usuarios</h1>
+        <ul className="app__users">
+          {userData.map((user, index) => {
+            return (
+              <li className="user" key={index}>
+                <div className="user__info">
+                  <h2 className="user__info-name">{user.name}</h2>
+                  <p className="user__info-email">{`Email: ${user.email}`}</p>
+                  <ul className="user__info-passwords">
+                    {user.passwords.map((passwords, pw) => {
+                      return (
+                        <li className="user__info-password" key={pw}>{passwords}</li>
+                      );
+                    })}
+                  </ul>
+                  <ul className="user__info-bank">
+                    <li className="user__info-bank-iban">{`IBAN: ${user.bank.iban}`}</li>
+                    <li className="user__info-bank-pin">{`Pin: ${user.bank.pin}`}</li>
+                  </ul>
+                </div>
+              </li>
+            );
+          })}   
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
