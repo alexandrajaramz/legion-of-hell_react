5f9e6124e66af7178df46d48da4be8ea328ba216
alexandrajaramz <alexandra.jara.mz@gmail.com>
2019-09-15 22:29:45 +0200

    fix structure in card

--- src/components/DataCard.js
+++ src/components/DataCard.js
@@ -3,8 +3,7 @@ import React from 'react';
 class DataCard extends React.Component {
     render() {
         return (
-            <React.Fragment>
-                <div className="user__info">
+            <div className="user__info">
                 <h2 className="user__info-name">{this.props.name}</h2>
                 <p className="user__info-email">{`Email: ${this.props.email}`}</p>
                 <ul className="user__info-passwords">
@@ -18,8 +17,7 @@ class DataCard extends React.Component {
                     <li className="user__info-bank-iban">{`IBAN: ${this.props.bank.iban}`}</li>
                     <li className="user__info-bank-pin">{`Pin: ${this.props.bank.pin}`}</li>
                 </ul>
-                </div>
-            </React.Fragment> 
+            </div> 
         );
     }
 }
