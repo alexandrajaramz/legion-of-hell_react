81e2adad062b32ab69e4972f5a0e9dc3f75bd58a
alexandrajaramz <alexandra.jara.mz@gmail.com>
2019-09-16 22:41:06 +0200

    create clock in the footer from task-2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,37 @@
 import React from 'react';
 
 class Footer extends React.Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          time: '00:00:00'
+        }
+    }
+    
+    componentDidMount() {
+        setInterval(()=>{
+            this.getTime();
+        }, 1000);
+    }
+    
+    getTime(){
+        const actualDate = new Date();
+
+        const hours = actualDate.getHours();
+        const minutes = actualDate.getMinutes();
+        const seconds = actualDate.getSeconds();
+        
+        this.setState({
+            time: `${hours}:${minutes}:${seconds}`
+        })
+    }
+
     render() {
         return (
-            <React.Fragment>
-                <footer className="page__footer">
-                    <p className="footer-text">loh();</p>
-                </footer>
-            </React.Fragment>
+            <footer className="page__footer">
+                <p className="footer-text">loh(); || {this.state.time}</p>
+            </footer>
         );
     }
 }
